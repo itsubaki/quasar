bcc511258b3b7f4f0baac5e72929b83982893da7
itsubaki <1759459+itsubaki@users.noreply.github.com>
2025-09-01 12:10:29 +0000

    Update client

--- client/client.go
+++ client/client.go
@@ -10,6 +10,22 @@ import (
 	"github.com/itsubaki/quasar/gen/quasar/v1/quasarv1connect"
 )
 
+type States struct {
+	States []State `json:"states"`
+}
+
+type State struct {
+	Amplitude    Amplitude `json:"amplitude"`
+	Probability  float64   `json:"probability"`
+	Int          []uint64  `json:"int"`
+	BinaryString []string  `json:"binary_string"`
+}
+
+type Amplitude struct {
+	Real float64 `json:"real"`
+	Imag float64 `json:"imag"`
+}
+
 type Client struct {
 	quasarClient quasarv1connect.QuasarServiceClient
 }
